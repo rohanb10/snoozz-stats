a11c47d9849938970764f6b73eff31a3bba38d95
rohan <rohanb10@gmail.com>
2021-06-11 18:50:35 +0530

    cors in da house

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-var express = require("express");
-var path = require("path");
-var bodyParser = require("body-parser");
-var mongodb = require("mongodb");
+var express = require('express');
+var path = require('path');
+var cors = require('cors');
+var bodyParser = require('body-parser');
+var mongodb = require('mongodb');
 var ObjectID = mongodb.ObjectID;
 
-var COLLECTION = "clicks";
+var COLLECTION = 'clicks';
 const valid_options = [
   'startup',
   'in-an-hour',
@@ -17,11 +18,12 @@ const valid_options = [
   'week',
   'month',
   'custom'
-]
+];
 
 var app = express();
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
+app.use(cors())
 
 // Create a database variable outside of the database connection callback to reuse the connection pool in your app.
 var db;
@@ -35,36 +37,35 @@ mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
 
   // Save database object from the callback for reuse.
   db = database.db('stats');
-  console.log("Database connection ready");
+  console.log('Database connection ready');
 
   // Initialize the app.
   var server = app.listen(process.env.PORT || 8080, function () {
     var port = server.address().port;
-    console.log("App now running on port", port);
+    console.log('App now running on port', port);
   });
 });
 
 // Generic error handler used by all endpoints.
 function handleError(res, reason, message, code) {
-  console.log("ERROR: " + reason);
+  console.log('ERROR: ' + reason);
   res.status(code || 500).json({why: message});
 }
 
-app.get("/clicks", function(req, res) {
+app.get('/clicks', function(req, res) {
   db.collection(COLLECTION).find({}).toArray(function(err, docs) {
-    if (err) return handleError(res, err.message, "Failed to get clicks.");
+    if (err) return handleError(res, err.message, 'Failed to get clicks.');
     res.status(200).json(docs);
   });
 });
 
-app.post("/clicks", function(req, res) {
-  try {console.log('ROHANROHANROHANROHANROHAN',req.query, typeof req.query.o, valid_options.includes(req.query.o))} catch{}
+app.post('/clicks', function(req, res) {
   if (!req || !req.query || !req.query.o || typeof req.query.o !== 'string' || !valid_options.includes(req.query.o)) {
     return handleError(res, 'Bad Input', 'Get out of my swamp', 400);
   }
 
   db.collection(COLLECTION).update({option: req.query.o}, {$inc: {count: 1}}, function(err, doc) {
     if (err) return handleError(res, err.message, 'Failed to save choice');
-    res.status(200).json(doc);
+    res.status(200).json({result: 'You did it!'});
   })
 });
