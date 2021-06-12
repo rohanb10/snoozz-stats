2725b9160c985245038d74f293379c0b84338a8a
rohan <rohanb10@gmail.com>
2021-06-12 14:13:04 +0530

    cors localhost

--- server.js
+++ server.js
@@ -5,7 +5,38 @@ var bodyParser = require('body-parser');
 var mongodb = require('mongodb');
 var ObjectID = mongodb.ObjectID;
 
-var COLLECTION = 'clicks';
+var db, COLLECTION = 'clicks', app = express();
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(cors({methods: ['GET', 'POST']}))
+
+mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+
+  db = database.db('stats');
+  console.log('Database connection ready');
+
+  var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log('App now running on port', port);
+  });
+});
+
+function handleError(res, msg, code) {
+  console.log('ERROR: ' + msg);
+  res.status(code || 500).json({why: msg});
+}
+
+app.get('/clicks', cors({methods: ['GET'], origin: ['https://snoozz.me', 'http://127.0.0.1:9999']}) function(req, res) {
+  db.collection(COLLECTION).find({}, {_id: 0}).toArray(function(err, docs) {
+    if (err) return handleError(res, 'No Stats 4 U.', 503);
+    res.status(200).json(docs);
+  });
+});
+
 const valid_options = [
   'startup',
   'in-an-hour',
@@ -20,46 +51,7 @@ const valid_options = [
   'custom'
 ];
 
-var app = express();
-app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.json());
-app.use(cors({methods: ['GET', 'POST']}))
-
-// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
-var db;
-
-// Connect to the database before starting the application server.
-mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
-  if (err) {
-    console.log(err);
-    process.exit(1);
-  }
-
-  // Save database object from the callback for reuse.
-  db = database.db('stats');
-  console.log('Database connection ready');
-
-  // Initialize the app.
-  var server = app.listen(process.env.PORT || 8080, function () {
-    var port = server.address().port;
-    console.log('App now running on port', port);
-  });
-});
-
-// Generic error handler used by all endpoints.
-function handleError(res, msg, code) {
-  console.log('ERROR: ' + msg);
-  res.status(code || 500).json({why: msg});
-}
-
-app.get('/clicks', function(req, res) {
-  db.collection(COLLECTION).find({}).toArray(function(err, docs) {
-    if (err) return handleError(res, 'No Stats 4 U.', 503);
-    res.status(200).json(docs);
-  });
-});
-
-app.post('/clicks', function(req, res) {
+app.post('/clicks', cors({methods: ['POST']}), function(req, res) {
   if (!req || !req.body || !req.body.o || !req.body.o.length || typeof req.body.o !== 'string' || !valid_options.includes(req.body.o)) {
     return handleError(res,'Get your shit inputs out of my swamp', 418);
   }
