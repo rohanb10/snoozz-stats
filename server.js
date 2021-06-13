805a4162474c6670bb9d0ffbde93fd3ffc555bbc
rohan <rohanb10@gmail.com>
2021-06-13 23:38:00 +0530

    test ing

--- server.js
+++ server.js
@@ -24,18 +24,18 @@ mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
   });
 });
 
-function handleError(res, msg, code) {
+function handleError(res, msg) {
   console.log('ERROR: ' + msg);
-  res.status(code || 500).json({why: msg});
+  res.status(204).json({why: msg});
 }
 
 app.get('/clicks', cors({methods: ['GET']}), function(req, res) {
   console.log('ORIGIN ', req.headers.origin);
   if (!req || !req.headers || !req.headers.origin || !['https://snoozz.me', 'http://127.0.0.1:9000'].includes(req.headers.origin)) {
-    return handleError('Who are you?', 403)
+    return handleError(res, 'Who are you?')
   }
   db.collection(COLLECTION).find({}, {_id: 0}).toArray(function(err, docs) {
-    if (err) return handleError(res, 'No Stats 4 U.', 200);
+    if (err) return handleError(res, 'No Stats 4 U.');
     res.status(200).json(docs);
   });
 });
@@ -57,10 +57,10 @@ const valid_options = [
 app.post('/clicks', cors({methods: ['POST']}), function(req, res) {
   console.log('ORIGIN ', req.headers.origin);
   if (!req || !req.headers || !req.headers.origin || !/^(safari-web\-|chrome\-|moz\-)?extension:\/\/.*/.test(req.headers.origin)) {
-    return handleError(res, 'I dont even know who you are', 403)
+    return handleError(res, 'I dont even know who you are')
   }
   if (!req.body || typeof req.body !== 'string' || !req.body.length || !valid_options.includes(req.body)) {
-    return handleError(res, 'Get out of my swamp', 418);
+    return handleError(res, 'Get out of my swamp');
   }
 
   db.collection(COLLECTION).update({option: req.body}, {$inc: {count: 1}}, function(err, doc) {
