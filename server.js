2a2dbe4111df3d84c9c1982251af5a976dee0962
rohan <rohanb10@gmail.com>
2021-06-13 23:53:28 +0530

    im ready for prod

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var bodyParser = require('body-parser');
 var mongodb = require('mongodb');
 var ObjectID = mongodb.ObjectID;
 
-var db, COLLECTION = 'clicks', app = express();
+var db, C = 'clicks', app = express();
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.text({limit: '1kb'}));
 
@@ -24,23 +24,24 @@ mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
   });
 });
 
-function handleError(res, msg) {
+function reject(res, msg) {
   console.log('ERROR: ' + msg);
   res.status(204).json({why: msg});
 }
 
 app.get('/clicks', cors({methods: ['GET']}), function(req, res) {
-  console.log('ORIGIN ', req.headers.origin);
   if (!req || !req.headers || !req.headers.origin || !['https://snoozz.me', 'http://127.0.0.1:9000'].includes(req.headers.origin)) {
-    return handleError(res, 'Who are you?')
+    return reject(res, 'Who are you?')
   }
-  db.collection(COLLECTION).find({}, {_id: 0}).toArray(function(err, docs) {
-    if (err) return handleError(res, 'No Stats 4 U.');
+  db.collection(C).find({}, {_id: 0}).toArray(function(err, docs) {
+    if (err) {
+      return reject(res, 'No Stats 4 U.');
+    }
     res.status(200).json(docs);
   });
 });
 
-const valid_options = [
+const valid = [
   'startup',
   'in-an-hour',
   'today-morning',
@@ -55,16 +56,17 @@ const valid_options = [
 ];
 
 app.post('/clicks', cors({methods: ['POST']}), function(req, res) {
-  console.log('ORIGIN ', req.headers.origin);
-  if (!req || !req.headers || !req.headers.origin || !/^(safari-web\-|chrome\-|moz\-)?extension:\/\/.*/.test(req.headers.origin)) {
-    return handleError(res, 'I dont even know who you are')
+  if (!req || !req.headers || !req.headers.origin || !/^(safari-web\-|chrome\-|moz\-)?extension:\/\/.*/.test(req.headers.origin)){
+    return reject(res, 'I dont even know who you are')
   }
-  if (!req.body || typeof req.body !== 'string' || !req.body.length || !valid_options.includes(req.body)) {
-    return handleError(res, 'Get out of my swamp');
+  if (typeof req.body !== 'string' || !valid.includes(req.body)) {
+    return reject(res, 'Get out of my swamp');
   }
 
-  db.collection(COLLECTION).update({option: req.body}, {$inc: {count: 1}}, function(err, doc) {
-    if (err) return handleError(res, 'I dont want it');
+  db.collection(C).update({option: req.body}, {$inc: {count: 1}}, function(err, doc) {
+    if (err) {
+      return reject(res, 'I dont want it');
+    }
     res.status(200).json({nice: 'Noice'});
   })
 });
