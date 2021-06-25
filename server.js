3fd96fd719a5ec8be9b1d4142fcc8038fadf0f6d
rohan <rohanb10@gmail.com>
2021-06-25 22:30:36 +0530

    logs logs logs dogs

--- server.js
+++ server.js
@@ -6,46 +6,6 @@ var mongodb = require('mongodb');
 var ObjectID = mongodb.ObjectID;
 
 var db, C = 'clicks', T = 'times', app = express();
-app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.text({limit: '1kb'}));
-
-mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
-  if (err) {
-    console.log(err);
-    process.exit(1);
-  }
-
-  db = database.db('stats');
-  console.log('Database connection ready');
-  var server = app.listen(process.env.PORT || 8080, function () {
-    console.log('App now running on port', server.address().port);
-  });
-});
-
-function reject(res, msg) {
-  console.log('ERROR: ' + msg);
-  res.status(204).json({why: msg});
-}
-
-app.get('/clicks', cors({methods: ['GET']}), function(req, res) {
-  if (!req || !req.headers || !req.headers.origin || !['https://snoozz.me', 'http://127.0.0.1:9000'].includes(req.headers.origin)) {
-    return reject(res, 'Who are you?')
-  }
-  db.collection(C).find({}, {projection: {_id: 0}}).toArray(function(err, docs) {
-    if (err) return reject(res, 'No Clicks 4 U.');
-    res.status(200).json(docs);
-  });
-});
-
-app.get('/times', cors({methods: ['GET']}), function(req, res) {
-  if (!req || !req.headers || !req.headers.origin || !['https://snoozz.me', 'http://127.0.0.1:9000'].includes(req.headers.origin)) {
-    return reject(res, 'Who are you?');
-  }
-  db.collection(T).find({}, {projection: {_id: 0}}).toArray(function(err, docs) {
-    if (err) return reject(res, 'No Times 4 U.');
-    res.status(200).json(docs);
-  });
-});
 
 var validChoices = [
   'startup',
@@ -60,38 +20,79 @@ var validChoices = [
   'month',
   'custom'
 ];
+var trusted = ['https://snoozz.me', 'http://127.0.0.1:9000'];
+var extensions = new RegExp('^(safari-web\-|chrome\-|moz\-)?extension:\/\/.*');
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.text({limit: '1kb'}));
+
+mongodb.MongoClient.connect(process.env.MONGODB_URI, {useUnifiedTopology: true}, (err, database) => {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+
+  db = database.db('stats');
+  var server = app.listen(process.env.PORT || 8080, _ => console.log('Hello there. How nice of you to join us port ' + server.address().port));
+});
+
+function reject(res, where, msg) {
+  res.status(204).json({why: msg});
+  console.log(where + ' ERROR - ' + msg);
+}
+
+app.get('/clicks', cors({methods: ['GET']}), (req, res) => {
+  if (!req || !req.headers || !req.headers.origin || !trusted.includes(req.headers.origin)) {
+    return reject(res, 'GET CLICKS: ', 'Who are you? ' + req.headers.origin);
+  }
+  db.collection(C).find({}, {projection: {_id: 0}}).toArray((err, docs) => {
+    if (err) return reject(res, 'GET CLICKS: ', 'No Clicks 4 U.');
+    res.status(200).json(docs);
+  });
+});
+
+app.get('/times', cors({methods: ['GET']}), (req, res) => {
+  if (!req || !req.headers || !req.headers.origin || !trusted.includes(req.headers.origin)) {
+    return reject(res, 'GET TIMES:  ', 'Who are you? ' + req.headers.origin);
+  }
+  db.collection(T).find({}, {projection: {_id: 0}}).toArray((err, docs) => {
+    if (err) return reject(res, 'GET TIMES:  ', 'No Times 4 U.');
+    res.status(200).json(docs);
+  });
+});
 
 // round time to nearest 15 mins
 function calcTime(num) {
-  var pad = n => n < 10 ? '0' + n : '' + n;
+  var pad = n => (n < 10 ? '0' : '') + n;
   var [h, m] = num.toString().match(/\d{2}/g).map(n => parseInt(n));
-  if (Math.round(m/15) == 4) h = (h + 1) % 24;
+  h = (Math.round(m/15) == 4) ? (h + 1) % 24 : h;
   m = (Math.round(m/15) * 15) % 60;
   return pad(h) + pad(m);
 }
 
 app.post('/clicks', cors({methods: ['POST']}), function(req, res) {
-  if (!req || !req.headers || !req.headers.origin || !/^(safari-web\-|chrome\-|moz\-)?extension:\/\/.*/.test(req.headers.origin)) {
-    if (req.headers && req.headers.origin) console.log(req.headers.origin);
-    return reject(res, 'This is not the server you are looking for');
+  if (!req || !req.headers || !req.headers.origin || !extensions.test(req.headers.origin)) {
+    var intruder = (req.headers && req.headers.origin) ? req.headers.origin : 'stranger danger';
+    return reject(res, 'POST CLICKS:', 'This is not the server you are looking for | ' + intruder);
   }
-  console.log('ANUSTAT: ' + req.body);
   if (typeof req.body !== 'string') {
-    reject(res, 'Dont send me this garbage');
+    reject(res, 'POST CLICKS:', 'Dont send me this garbage');
   } else if (validChoices.includes(req.body)) {
     db.collection(C).updateOne({option: req.body}, {$inc: {count: 1}}, function(err, doc) {
-      if (err) return reject(res, 'I dont want it');
+      if (err) return reject(res, 'POST CLICKS', 'I dont want it | ' + req.body);
       res.status(200).json({nice: 'Noice'});
+      console.log('OLD STAT:   ' + req.body);
     });
   } else if (req.body.indexOf('.') > -1 && req.body.split('.').length === 2) {
     var [choice, time] = req.body.split('.');
     if (/^([0-1][0-9]|[2][0-3])[0-5][0-9]$/.test(time) && validChoices.includes(choice)) {
       db.collection(T).updateOne({when: calcTime(time)}, {$inc: {count: 1}});
       db.collection(C).updateOne({option: choice}, {$inc: {count: 1}});
-      return res.status(200).json({nice: 'Noice'});
+      res.status(200).json({nice: 'Noice'});
+      console.log('NEW STAT:   ' + req.body + ' | ' + choice + ' ' + calcTime(time));
     }
-    reject(res, 'We dont like your type here');
+    reject(res, 'POST CLICKS:', 'We dont like your type here | ' + choice + ' ' + time);
   } else {
-    reject(res, 'Get out of my swamp');
+    reject(res, 'POST CLICKS:', 'Get out of my swamp');
   }
 });
